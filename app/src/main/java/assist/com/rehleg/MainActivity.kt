ee1481be81905af4fabaf9e2f25c6af5918c19d0
mihai <cypryjefhardy@gmail.com>
2017-10-30 14:03:38 +0200

    Project setup

---
+++ app/src/main/java/assist/com/rehleg/MainActivity.kt
@@ -0,0 +1,12 @@
+package assist.com.rehleg
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
